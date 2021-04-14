b1f7c3b4dec221269b9ea74ad8789842a9ca0eaf
ok97 <omkhawshi0@gmail.com>
2021-04-15 00:05:12 +0530

    Added :Added: Snake and Ladder Game checks for a Option. They are No Play

--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -5,17 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SnakeAndLadder
-{  /* UC2 :- The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number between 1 to 6
-     */
+{  /* UC3 :- The Player then checks for a Option. They are No Play,
+             Ladder or Snake. - Use ((RANDOM)) to check for Options - In Case of No Play the player stays in the same
+             position
+             - In Case of Ladder the player moves ahead by the number of position received in the die 
+             - In Case of Snake the player moves behind by the
+
+    */
     class SnakeAndLadder
     {
         int START_POSITION = 0; // Declare integer start position in 0
+        const int NoPlay = 0;
+        const int Ladder = 1;
+        const int Snakle = 2;
         public void PlayGame()
         {
-            Random random = new Random();
-            int Die = random.Next(1, 6);    //Random Value Generate 1 to 6
-            Console.WriteLine(" Player Die Roll Position :-" + Die);        //Print Random value
-            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION); //Print Start Position
+            Random random = new Random(); //Generate random integers in range 0 to 6
+            int Die = random.Next(1, 6);    // Random Value Generate 1 to 6
+            int Option = random.Next(0, 3); // Random generate 0 to 3 value
+            int Player = 0;
+            switch (Option) // use switch casw for option Random value
+            {
+                case NoPlay:
+                    Console.WriteLine("No Play, Pass the chance"); // Print  
+                    break;
+                case Ladder:
+                    Player = Die; // 
+                    Console.WriteLine("You got Ladder \n Player position = " + Player); // Print player value random genrate
+                    break;
+                case Snakle:
+                    Player = Die;
+                    Console.WriteLine("You gotSnake \n player position = " + Player); // Print player value random genrate
+                    break;
+            }
+            Console.WriteLine(" Player Die Roll Position :- " + Die);                   //Print Random value
+            Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);     //Print Start Position
             Console.ReadLine();
         }
         static void Main(string[] args)//Main method
