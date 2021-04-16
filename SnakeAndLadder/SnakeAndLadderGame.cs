0d1fe5bcc6953e6baf9f252d28e683936a9861e7
ok97 <omkhawshi0@gmail.com>
2021-04-17 01:19:05 +0530

    Added: Play the game with 2 Player

--- SnakeAndLadder/SnakeAndLadderGame.cs
+++ SnakeAndLadder/SnakeAndLadderGame.cs
@@ -5,24 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace SnakeAndLadder
-{  /* UC6:- Report the number of times the dice was played to win the game and also the position after every die role.
-
+{  /* UC7:- Play the game with 2 Player. In this case if a Player gets a Ladder then plays again.
+            Finally report which Player won the game
     */
     class SnakeAndLadder
     {
         int START_POSITION = 0; // Declare integer start position in 0
-        const int NoPlay = 0;  
+        const int NoPlay = 0;
         const int Ladder = 1;
         const int Snakle = 2;
         const int winning_position = 100; // Declare Winning Position 100
         public void PlayGame()
         {
+            int[] Player = new int[2] { 0, 0 };
             int Dice_Count = 0;
-            int Player = 0;
-            while (Player < winning_position)
+            int Chance = 0;
+            while (Player[Chance] < winning_position)
             {
                 Random random = new Random(); //Generate random integers in range 0 to 6
-                 int Die = random.Next(1, 6);    // Random Value Generate 1 to 6
+                int Die = random.Next(1, 6);    // Random Value Generate 1 to 6
                 int Option = random.Next(0, 3); // Random generate 0 to 3 value
                 Dice_Count++; //Count the Dice
                 Console.WriteLine($"Report the number of times the dice was played to win {Dice_Count}");
@@ -34,25 +35,45 @@ namespace SnakeAndLadder
                         Console.WriteLine("No Play, Pass the chance"); // Print  
                         break;
                     case Ladder:
-                        Player += Die; 
-                        Console.WriteLine("You got Ladder \n Player position = " + Player); // Print player value random genrate
+                        if(Player[Chance]+Die<=winning_position);
+                        {
+                            Player[Chance] += Die;
+
+                        }
+                        Console.WriteLine("You got Ladder \n Player position = " + Chance+"--->"+"Position"+" "+Player[Chance]); // Print player value random genrate
                         break;
                     case Snakle:
-                        Player -= Die;
-                        if (Player < START_POSITION) //Check Lessthan Start Position
+                        if(Player[Chance] - Die<START_POSITION)
                         {
-                            Player = START_POSITION; // Player value = Start Value
+                            Player[Chance] = START_POSITION;
                         }
-                        Console.WriteLine("You gotSnake \n player position = " + Player);
+                        else //(Player < START_POSITION) //Check Lessthan Start Position
+                        {
+                            Player[Chance] -= Die; // Player value = Start Value
+                        }
+                        Console.WriteLine("You gotSnake \n player = "+Chance+ "---> position "+" " + Player[Chance]);
 
                         break;
+                    default:
+                        break;
+                }
+                if(Option==NoPlay||Option==Snakle)
+                {
+                    if(Chance==0)
+                    {
+                        Chance = 1;
+                    }
+                    else
+                    {
+                        Chance = 0;
+                    }
                 }
 
             }
-            Console.WriteLine($"Position After every Dice role {Dice_Count}"); //Print Dice 
-          // Console.WriteLine(" Player Die Roll Position :- " + Die);                   //Print Random value
-           // Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);     //Print Start Position
-            Console.ReadLine();
+            Console.WriteLine($"Total number of Times Dice role {Dice_Count}"); //Print Dice 
+                                                                               // Console.WriteLine(" Player Die Roll Position :- " + Die);                   //Print Random value
+                                                                               // Console.WriteLine(" Single Player Start_Position:- " + START_POSITION);     //Print Start Position
+            
         }
         static void Main(string[] args)//Main method
         {
